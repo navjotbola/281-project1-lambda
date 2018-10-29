a5737ef637d924e732b859daa1a988aa2aab8d52
Navjot Bola <navjotb1@gmail.com>
2018-10-29 13:08:21 -0700

    Fixing delete logic

--- deleteFileAdmin.js
+++ deleteFileAdmin.js
@@ -2,9 +2,11 @@ import * as dynamoDbLib from "./utils/dynamodb-lib";
 import { success, failure } from "./utils/response-lib";
 
 export async function main(event, context, callback) {
+    const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
         Key: {
+            userId: data.userId,
             fileId: event.pathParameters.id
         }
     };
